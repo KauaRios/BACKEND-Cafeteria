9d0e8de783129203d983db18143c7940317b8761
KauaRios <kauarios49@gmail.com>
2025-11-13 13:27:44 -0300

    Subindo projeto Docker PHP + MySQL com Login funcionando

--- config/funcoes.php
+++ config/funcoes.php
@@ -1,11 +1,12 @@
 <?php
 // config/funcoes.php
 
-
+// Função para registrar logs tanto no banco de dados quanto em arquivo
 function registrar_log(PDO $pdo, string $nivel, string $mensagem, ?array $contexto = null, ?int $id_usuario = null) {
     
     // 1. SALVA NO BANCO DE DADOS 
     try {
+        // Converte o contexto em JSON
         $contexto_str = $contexto ? json_encode($contexto, JSON_UNESCAPED_UNICODE) : null;
 
       
@@ -26,28 +27,35 @@ function registrar_log(PDO $pdo, string $nivel, string $mensagem, ?array $contex
 
   
     try {
+        //  SALVA EM ARQUIVO DE LOG
         $logDir = __DIR__ . '/../logs';
+        // Define o caminho do arquivo de log
         $logFile = $logDir . '/app.log';
 
+
+        // Cria o diretório de logs se não existir
         if (!is_dir($logDir)) {
             mkdir($logDir, 0777, true);
         }
 
         $dataHora = date('Y-m-d H:i:s');
         
-      
+      //se id_usuario for passado, inclui no log
         $userStr = $id_usuario ? "[User ID: $id_usuario]" : "[Sistema]";
-        
+        // Monta a linha do log
         $linhaLog = "[$dataHora] [$nivel] $userStr $mensagem";
         
+        // Adiciona contexto se houver
         if ($contexto) {
             $linhaLog .= " | Dados: " . json_encode($contexto, JSON_UNESCAPED_UNICODE);
         }
+        // Quebra de linha
         $linhaLog .= PHP_EOL;
-
+        // Escreve no arquivo
         file_put_contents($logFile, $linhaLog, FILE_APPEND);
-        
-    } catch (Exception $e) {
+    
+    } 
+    catch (Exception $e) {
         // Ignora erro de arquivo
     }
 }
